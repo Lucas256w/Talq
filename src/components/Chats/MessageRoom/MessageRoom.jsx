f0532f64ccc1e64772235367b120577c95253707
Jiahao <lucashuamani256w@gmail.com>
2024-03-22 11:58:27 -0400

    Add input field to message room

--- src/components/Chats/MessageRoom/MessageRoom.jsx
+++ src/components/Chats/MessageRoom/MessageRoom.jsx
@@ -10,14 +10,16 @@ const testData = [
     content:
       "Thank you for the offer you gave me. yes I will accept the project from you. for the brief please send it now so I can study it first.Thank you for the offer you gave me. yes I will accept the project from you. for the brief please send it now so I can study it first.Thank you for the offer you gave me. yes I will accept the project from you. for the brief please send it now so I can study it first.Thank you for the offer you gave me. yes I will accept the project from you. for the brief please send it now so I can study it first.Thank you for the offer you gave me. yes I will accept the project from you. for the brief please send it now so I can study it first.",
     time: "12.00 PM",
+    uid: 2,
   },
   {
     id: 1,
     content:
       "Good afternoon. may I ask your help to make me a real estate landing page. for more details I will send as soon as you approve it. thanks!",
     time: "12.00 PM",
+    uid: 3,
   },
-  { id: 2, content: "Hello, Mike", time: "12.00 PM" },
+  { id: 2, content: "Hello, Mike", time: "12.00 PM", uid: 4 },
 ];
 
 const MessageRoom = () => {
@@ -38,14 +40,14 @@ const MessageRoom = () => {
       <div className={styles.room} ref={bottomRef}>
         {testData.map((message) =>
           message.id % 2 === 1 ? (
-            <div key={testData.content} className={styles.receiver}>
+            <div key={message.uid} className={styles.receiver}>
               <div className={styles.receiverBox} key={message.id}>
                 <div>{message.content}</div>
               </div>
               <div className={styles.receiverTime}>{message.time}</div>
             </div>
           ) : (
-            <div key={testData.content} className={styles.sender}>
+            <div key={message.uid} className={styles.sender}>
               <div className={styles.senderBox} key={message.id}>
                 <div>{message.content}</div>
               </div>
@@ -53,6 +55,15 @@ const MessageRoom = () => {
             </div>
           )
         )}
+      </div>
+      <div className={styles.bottom}>
+        <div className={styles.inputContainer}>
+          <input
+            type="text"
+            className={styles.input}
+            placeholder="Send message..."
+          />
+        </div>
       </div>
     </div>
   );
